c20ccd0b09f7896b166115b6831721945ffe592c
Paulina Kimak <p.kimak.047@studms.ug.edu.pl>
2025-10-24 23:06:16 +0200

    Update trigger exec

--- Komis/project_package/trigger_ex.sql
+++ Komis/project_package/trigger_ex.sql
@@ -1,54 +1,41 @@
------------------------TRIGGER 1 - PRZYKŁAD (historia zmian ceny samochodu )
---Dodanie nowego samochodu do bazy danych:
-INSERT INTO samochod (nr_rejestracyjny, nr_vin, marka, model, rocznik, przebieg, silnik, paliwo, moc, kolor, rodzaj_pojazdu, ladownosc, gotowy_do_sprzedaży, cena, opis, id_plac)
-VALUES ('ABC1234', '1HGBH41JXMN109186', 'Toyota', 'Camry', 2020, 15000.00, 2.5, 'Benzyna', 178, 'Biały', 'Sedan', 1.50, TRUE, 85000.00, 'Samochód w bardzo dobrym stanie', 1);
+-----------------------TRIGGER 1 -----------------------------------------
 
---Aktualizacja ceny samochodu:
+-- Sprawdź obecną cenę samochodu 1
+SELECT id_samochod, cena FROM samochod WHERE id_samochod = 1;
+
+-- Zmień cenę
 UPDATE samochod
-SET cena = 80000.00
-WHERE id_samochod = 16;
+SET cena = cena + 5000
+WHERE id_samochod = 1;
 
---Weryfikacja zmian cen w tabeli historia_cen
-SELECT * FROM historia_cen
-WHERE id_samochod = 16;
-
+-- Sprawdź historię cen
+SELECT * FROM historia_cen WHERE id_samochod = 1 ORDER BY data_zmiany DESC;
 
 
------------------------TRIGGER 2 - PRZYKŁAD -----zablokowanie wstawienia rekordu -----------------------------------------------
+-----------------------TRIGGER 2 -----------------------------------------
 
+-- Spróbuj dodać transakcję dla samochodu, który nie jest gotowy do sprzedaży
+INSERT INTO kartoteka_transakcji 
+(rodzaj, data_transakcji, samochod_w_rozliczeniu, uwagi, id_samochod, id_klient, id_sprzedawca, id_plac, id_faktura)
+VALUES 
+('sprzedaż', CURRENT_DATE, false, 'Test triggera', 3, 1, 1, 1, 1);
 
---Wstawienie nowej transakcji, które uruchomi trigger before_insert_kartoteka_transakcji. 
---Zakładamy, że mamy samochód o id_samochod = 1, który nie jest gotowy do sprzedaży:
+------------------------TRIGGER 3 -----------------------------------------
+-- Dodanie nowej dostawy
+INSERT INTO dostawa (data_dostawy, kraj_pochodzenia, czy_zarejestrowany, czy_uszkodzony, id_plac, id_samochod)
+VALUES (CURRENT_DATE, 'Polska', true, false, 1, 11);
 
--- Ustawienie statusu gotowości samochodu na false
-UPDATE samochod SET gotowy_do_sprzedaży = FALSE WHERE id_samochod = 1;
+-- Sprawdź czy samochód został oznaczony jako gotowy
+SELECT id_samochod, gotowy_do_sprzedaży FROM samochod WHERE id_samochod = 11;
 
--- Próba wstawienia transakcji dla samochodu, który nie jest gotowy do sprzedaży
-INSERT INTO kartoteka_transakcji (rodzaj, data_transakcji, samochod_w_rozliczeniu, id_samochod, id_klient, id_sprzedawca, id_plac, id_faktura)
-VALUES ('Sprzedaż', '2024-05-01', FALSE, 1, 1, 1, 1, 1);
+------------------------TRIGGER 4----------------------------------
+DELETE FROM samochod WHERE id_samochod = 2;
 
--- Sprawdzenie, czy transakcja została wstawiona
-SELECT * FROM kartoteka_transakcji WHERE id_samochod = 1;
+-------------------------TRIGGER 5---------------------------------
+-- Dodanie nowego ubezpieczenia
+INSERT INTO ubezpieczenie (id_samochod, firma_ubezpieczeniowa, nr_polisy, data_poczatku, data_konca, koszt)
+VALUES (1, 'Allianz', 'POLISA123', CURRENT_DATE, CURRENT_DATE + INTERVAL '1 year', 1500);
 
---Ponieważ samochód o id_samochod = 1 nie jest gotowy do sprzedaży, 
---trigger before_insert_kartoteka_transakcji zablokuje wstawienie rekordu 
---i zwróci błąd: Samochód nie jest gotowy do sprzedaży.
-
---------------------------------------wstawienie nowego rekordu transakcji do tabeli kartoteka_transakcji-----------------------------------
-
---Teraz ustawimy samochód jako gotowy do sprzedaży i spróbujemy ponownie:
-
--- Ustawienie statusu gotowości samochodu na true
-UPDATE samochod SET gotowy_do_sprzedaży = TRUE WHERE id_samochod = 1;
-
--- Ponowna próba wstawienia transakcji dla samochodu, który jest gotowy do sprzedaży
-INSERT INTO kartoteka_transakcji (rodzaj, data_transakcji, samochod_w_rozliczeniu, id_samochod, id_klient, id_sprzedawca, id_plac, id_faktura)
-VALUES ('Sprzedaż', '2024-05-01', FALSE, 1, 1, 1, 1, 1);
-
--- Sprawdzenie, czy transakcja została wstawiona
-SELECT * FROM kartoteka_transakcji WHERE id_samochod = 1;
-
---Po ustawieniu statusu samochodu na gotowy do sprzedaży, 
---trigger pozwoli na wstawienie nowego rekordu transakcji do tabeli kartoteka_transakcji.
-
+-- Sprawdź wpis
+SELECT * FROM ubezpieczenie WHERE id_samochod = 1 ORDER BY id_ubezpieczenie DESC;
 
